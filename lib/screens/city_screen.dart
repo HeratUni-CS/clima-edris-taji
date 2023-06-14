83e6e415c17ff8af8193a646e639c61f28101f4c
Edria Taji <116660805+edris-taji@users.noreply.github.com>
2023-06-14 11:00:26 +0430

    My last changes

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -7,45 +7,45 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+  String cityName;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/city_background.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        constraints: BoxConstraints.expand(),
-        child: SafeArea(
-          child: Column(
-            children: <Widget>[
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('images/city_background.jpg'),
+              fit: BoxFit.cover,
+            )),
+            constraints: BoxConstraints.expand(),
+            child: SafeArea(
+                child: Column(children: <Widget>[
               Align(
-                alignment: Alignment.topLeft,
-                child: TextButton(
-                  onPressed: () {},
-                  child: Icon(
-                    Icons.arrow_back_ios,
-                    size: 50.0,
-                  ),
-                ),
-              ),
+                  alignment: Alignment.topLeft,
+                  child: FlatButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: 50.0,
+                      ))),
               Container(
-                padding: EdgeInsets.all(20.0),
-                child: null,
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Get Weather',
-                  style: kButtonTextStyle,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+                  padding: EdgeInsets.all(20.0),
+                  child: TextField(
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                      decoration: kTextFieldInputDecoration,
+                      onChanged: (value) {
+                        cityName = value;
+                      })),
+              FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context, cityName);
+                  },
+                  child: Text('weather', style: kButtonTextStyle))
+            ]))));
   }
 }
